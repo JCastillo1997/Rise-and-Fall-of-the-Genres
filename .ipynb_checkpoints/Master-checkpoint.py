fc2db9b24942503a35b8056a9cee26e83950a472
Tu Nombre <castillof1997@gmail.com>
2024-01-30 16:18:56 +0100

    Finally we can start exploring the imported data from the function

---
+++ .ipynb_checkpoints/Master-checkpoint.py
@@ -0,0 +1,29 @@
+def create_playlist_dataframe(playlist_id):
+
+    client_credentials_manager = SpotifyClientCredentials(client_id="8aaea3fe61404b23aa07c9f760df3985", client_secret="e6670aad9bc94f1ba66f382abe39394c")
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+    playlist = sp.playlist(playlist_id)
+
+
+  
+    data = {
+        'cancion':[],
+        'cantante': [],
+        'año': [],
+        'duracion_minutos': [],
+        'album': [],
+        'explicito': [],
+    }
+
+    for track in playlist['tracks']['items']:
+        data['cancion'].append(track['track']['name'])
+        data['cantante'].append(track['track']['artists'][0]['name'])
+        data['año'].append(track['track']['album']['release_date'][:4])
+        data['duracion_minutos'].append(track['track']['duration_ms'] / 60000)
+        data['album'].append(track['track']['album']['name'])
+        data['explicito'].append(track['track']['explicit'])
+
+    df = pd.DataFrame(data)
+    return df
